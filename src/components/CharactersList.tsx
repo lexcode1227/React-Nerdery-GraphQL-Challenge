794d3006d1aa246b9d98711a7c6a78eb0fd07f21
Henry Agustin <henryagustin297@gmail.com>
2024-12-20 15:23:48 -0600

    style: reorganize sections in mobile version

--- src/components/CharactersList.tsx
+++ src/components/CharactersList.tsx
@@ -54,7 +54,7 @@ const CharactersList: React.FC<CharacterListProps> = ({
   }, [handleScroll, loading, nextPage]);
 
   return (
-    <div className="h-screen overflow-y-auto">
+    <div className="h-screen overflow-y-auto order-2 md:order-none">
       {characters.map((character) => (
         <CharacterCard
           key={character.id}
