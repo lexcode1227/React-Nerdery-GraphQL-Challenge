35111b8999c2344adaddef5f9dc6c001f1eca73e
Henry Agustin <henryagustin297@gmail.com>
2024-12-24 16:56:12 -0600

    style: fix weird menu scroll

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import "./Loading.css";
 
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-[80vh]">
+    <div className="flex items-center justify-center h-[92vh]">
       <div className="loader"></div>
       <p className="text-base font-bold text-[#828282] ml-3">Loading</p>
     </div>
